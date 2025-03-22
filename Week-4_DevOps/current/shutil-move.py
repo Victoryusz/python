de3f6b20c4473b97f72f68defbca943802f1eada
Victoryusz <hugo4strong@gmail.com>
2025-03-22 20:09:10 -0300

    Adicinando shutil-move.py.

---
+++ Week-4_DevOps/current/shutil-move.py
@@ -0,0 +1,3 @@
+import shutil                                       # Importa a biblioteca shutil.
+
+shutil.move(src='source_dir/logs.txt', dst='mov_dir/logs_renomeado.txt')   # Move o arquivo logs.txt para mov_dir e renomeia.
