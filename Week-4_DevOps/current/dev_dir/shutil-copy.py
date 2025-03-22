98cca2604d158fbf591392026f53b21468a7f036
Victoryusz <hugo4strong@gmail.com>
2025-03-22 20:15:10 -0300

    Shutil Rmtree executado.

--- Week-4_DevOps/current/dev_dir/shutil-copy.py
+++
@@ -1,4 +0,0 @@
-import shutil                                        # Importa a biblioteca shutil.
-
-shutil.copy(src='logs.txt', dst='logs_copia.txt')    # Copia o conteúdo de logs.txt, CRIA um arquivo chamado logs_copia.txt com o conteúdo copiado.
-
